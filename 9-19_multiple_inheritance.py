9d471905bb5d71f955bb40bee6cf3a7309168f15
SHRUTIKA TAYDE <89908129+shrutikatayde@users.noreply.github.com>
2023-08-31 19:21:52 +0530

    Day 5

---
+++ 9-19_multiple_inheritance.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 30 09:12:21 2023
+
+@author: Lenovo
+"""
+#Multiple inheritance
+class Dance:
+    def __init__(self, typeOfDance):
+        self.typeOfDance= typeOfDance
+        
+class Dancer:
+    def __init__(self, name):
+        self.name= name
+        
+class DancerName(Dance, Dancer):
+    def __init__(self,typeOfDance, name):
+        self.typeOfDance= typeOfDance
+        self.name= name
+        
+obj= DancerName("Kathak", "Shruti")
+print(obj.name)
+print(obj.typeOfDance)
+print(DancerName.mro())
+        
+        
+        
+    
+        
+
+        
+
+        
