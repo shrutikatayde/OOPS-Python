3f3954da7833c1ac0618b2f36295130dce6f50e9
SHRUTIKA TAYDE <89908129+shrutikatayde@users.noreply.github.com>
2023-09-09 17:15:27 +0530

    Day 7

---
+++ 9-24_Exercise_4_text_speech_win32.py
@@ -0,0 +1,11 @@
+# pip install pypiwin32
+
+# text to speech coversion by using win32 API
+
+import win32com.client
+
+speak = win32com.client.Dispatch("SAPI.SpVoice")
+while 1:
+    print("Enter the string : ")
+    s = input()
+    speak.Speak(s)
