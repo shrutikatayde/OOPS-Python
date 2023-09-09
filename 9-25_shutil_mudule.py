2e16475398415936d43ea8b6f7e2c2811b597b29
SHRUTIKA TAYDE <89908129+shrutikatayde@users.noreply.github.com>
2023-09-09 18:45:44 +0530

    Day 7

---
+++ 9-25_shutil_mudule.py
@@ -0,0 +1,11 @@
+import shutil
+
+shutil.copy("Code_With_Harry.txt", "file.txt")
+
+# copy2 ---> more than copy like metadata
+# copytree ---> for copying directory
+# move ---> move the path
+# rmtree ---> delete directory
+
+import os
+# remove ---> remove file
