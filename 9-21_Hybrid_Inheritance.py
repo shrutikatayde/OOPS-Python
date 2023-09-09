7aa510b3813dc72572a27d4774d9210c603d4f3a
SHRUTIKA TAYDE <89908129+shrutikatayde@users.noreply.github.com>
2023-09-09 14:29:25 +0530

    Day 7

--- 9-21_Hybrid_Inheritance.py
+++ 9-21_Hybrid_Inheritance.py
@@ -1,29 +1,52 @@
-# Hybrid Inheritance
-
-
 class Engineering:
-    def __init__(self, year, branch):
+    def __init__(self, year, branch, experience):
         self.year = year
         self.branch = branch
+        self.experience = experience
 
     def student_details(self):
-        print(f"Name : {self.year}")
-        print(f"Branch : {self.branch}")
+        print(f"Year: {self.year}")
+        print(f"Branch: {self.branch}")
+
+    def faculty_details(self):
+        print(f"Year of experience: {self.experience}")
 
 
-class Student1(Engineering):
-    def __init__(self, name):
-        Engineering.__init__(self, year=4, branch="CSE")
+class Students(Engineering):
+    def __init__(self, name, year=3, branch="MEHC"):
+        super().__init__(
+            year, branch
+        )  # Call the parent class constructor directly with experience=0
         self.name = name
 
     def student_details(self):
-        print(f"Name : {self.year}")
-        print(f"Branch : {self.branch}")
+        super().student_details()
+        print(f"Name: {self.name}")
 
 
-class Student2(Engineering):
-    pass
+class Faculty(Engineering):
+    def __init__(self, t_name, year=7, branch="fghf", experience=10):
+        super().__init__(
+            year, branch, experience
+        )  # Call the parent class constructor directly
+        self.t_name = t_name
+
+    def faculty_details(self):
+        super().faculty_details()
+        print(f"Teacher Name: {self.t_name}")
 
 
-class SectionC(Student1, Student1):
-    pass
+class SectionC(Students, Faculty):
+    def __init__(self, subject):
+        Students.__init__(self, name="Shrutika")  # Pass name directly
+        Faculty.__init__(self, t_name="N. G. Rathi")  # Pass t_name directly
+        self.subject = subject
+
+    def section_c_details(self):
+        Students.student_details(self)
+        Faculty.faculty_details(self)
+        print(f"Subject: {self.subject}")
+
+
+result = SectionC("DBMS")
+result.section_c_details()
