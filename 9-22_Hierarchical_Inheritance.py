eba77c4c95404806a6df9d766710c26ad5d64d49
SHRUTIKA TAYDE <89908129+shrutikatayde@users.noreply.github.com>
2023-09-09 15:15:42 +0530

    Day 7

--- 9-22_Hierarchical_Inheritance.py
+++ 9-22_Hierarchical_Inheritance.py
@@ -2,20 +2,36 @@
 
 
 class A:
-    pass
+    def __init__(self, flower):
+        self.flower = flower
+
+    def display_flower(self):
+        print(f"Flower name : {self.flower}")
 
 
 class B(A):
-    pass
+    def __init__(self, flower, color):
+        super().__init__(flower)
+        self.color = color
+
+    def display_color(self):
+        super().display_flower()
+        print(f"Flower Color : {self.color}")
 
 
 class C(A):
-    pass
+    def __init__(self, flower, size):
+        A.__init__(self, flower)
+
+        self.size = size
+
+    def display_size(self):
+        print(f"Flower Size : {self.size}")
 
 
-class B1(B):
-    pass
+res = C("rose", "small")
+ress = B("Rose", "red")
 
 
-class B2(B):
-    pass
+ress.display_color()
+res.display_size()
