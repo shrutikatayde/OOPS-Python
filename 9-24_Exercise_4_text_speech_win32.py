6517c79ba29e187aafc867ef239627cbef6fa5d1
SHRUTIKA TAYDE <89908129+shrutikatayde@users.noreply.github.com>
2023-09-09 17:57:48 +0530

    Day 7

--- 9-24_Exercise_4_text_speech_win32.py
+++ 9-24_Exercise_4_text_speech_win32.py
@@ -7,5 +7,5 @@ import win32com.client
 speak = win32com.client.Dispatch("SAPI.SpVoice")
 while 1:
     print("Enter the string : ")
-    s = input()
-    speak.Speak(s)
+    str = input()
+    speak.Speak(str)
